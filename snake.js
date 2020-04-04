d394c72e70dd991f473cea473c634ab10258379d
Aditya Priyam <apriyam9@gmail.com>
2020-04-05 02:00:29 +0530

    adding tail to the snake

--- snake.js
+++ snake.js
@@ -1,16 +1,32 @@
 let s;
 const snakeScale = 20;
+let foodPosition;
 
 function setup() {
     createCanvas(600, 600);
     s = new Snake();
     frameRate(10);
+    pickLocation();
 }
 
 function draw() {
     background(51);
     s.update();
     s.show();
+
+    if (s.eat(foodPosition)) {
+        pickLocation();
+    }
+
+    fill(255, 0, 100);
+    rect(foodPosition.x, foodPosition.y, snakeScale, snakeScale);
+}
+
+function pickLocation() {
+    let cols = floor(width / snakeScale);
+    let rows = floor(height / snakeScale);
+    foodPosition = createVector(floor(random(cols)), floor(random(rows)));
+    foodPosition.mult(snakeScale);
 }
 
 function keyPressed() {
@@ -33,7 +49,15 @@ function Snake() {
     this.y = 0;
     this.xspeed = 1;
     this.yspeed = 0;
+    this.total = 0;
+    this.tail = [];
     this.update = function () {
+        if (this.total === this.tail.length) {
+            for (let i = 0; i < this.total; i++) {
+                this.tail[i] = this.tail[i + 1];
+            }
+        }
+        this.tail[this.total - 1] = createVector(this.x, this.y);
         this.x = this.x + this.xspeed * snakeScale;
         this.y = this.y + this.yspeed * snakeScale;
 
@@ -43,11 +67,25 @@ function Snake() {
 
     this.show = function () {
         fill(255);
+
+        for (let i = 0; i < this.total; i++) {
+            rect(this.tail[i].x, this.tail[i].y, snakeScale, snakeScale);
+        }
         rect(this.x, this.y, snakeScale, snakeScale);
     };
 
     this.direction = function (x, y) {
         this.xspeed = x;
         this.yspeed = y;
+    };
+
+    this.eat = function (pos) {
+        let distance = dist(this.x, this.y, pos.x, pos.y);
+        if (distance < 1) {
+            this.total++;
+            return true;
+        } else {
+            return false;
+        }
     };
 }
